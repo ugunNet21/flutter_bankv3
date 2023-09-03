f2c345be0046a800721878bca6a79ecf29d1cb6e
gugun gunawan <gugun.net21@gmail.com>
2023-09-03 20:10:34 +0700

    add sign up ktp page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bank_v2/ui/pages/onboarding_page.dart';
 import 'package:bank_v2/ui/pages/sign_in_page.dart';
 import 'package:bank_v2/ui/pages/sign_up_page.dart';
+import 'package:bank_v2/ui/pages/sign_up_set_ktp_page.dart';
 import 'package:bank_v2/ui/pages/sign_up_set_profile_page.dart';
 import 'package:bank_v2/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         '/sign-in': (context) => const SignInPage(),
         '/sign-up': (context) => const SignUpPage(),
         '/sign-up-set-profile': (context) => const SignUpSetProfilePage(),
+        '/sign-up-set-ktp': (context) => const SignUpSetKtpPage(),
       },
     );
   }
--- lib/ui/pages/sign_up_set_ktp_page.dart
+++ lib/ui/pages/sign_up_set_ktp_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 import '../../shared/theme.dart';
 import '../../widgets/buttons.dart';
 import '../../widgets/forms.dart';
 
-class SignUpSetProfilePage extends StatelessWidget {
-  const SignUpSetProfilePage({super.key});
+class SignUpSetKtpPage extends StatelessWidget {
+  const SignUpSetKtpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +29,7 @@ class SignUpSetProfilePage extends StatelessWidget {
             ),
           ),
           Text(
-            'Join Us to Unlock\nYour Growth',
+            'Verify Your\nAccount',
             style: blackTextStyle.copyWith(
               fontSize: 20,
               fontWeight: semibold,
@@ -47,38 +46,38 @@ class SignUpSetProfilePage extends StatelessWidget {
             ),
             child: Column(
               children: [
-                // Container(
-                //   width: 120,
-                //   height: 120,
-                //   decoration: BoxDecoration(
-                //     shape: BoxShape.circle,
-                //     color: lightBackgroundColor,
-                //   ),
-                //   child: Center(
-                //     child: Image.asset(
-                //       'assets/ic_upload.png',
-                //       width: 32,
-                //     ),
-                //   ),
-                // ),
                 Container(
                   width: 120,
                   height: 120,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/img_profile.png',
-                      ),
+                    color: lightBackgroundColor,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/ic_upload.png',
+                      width: 32,
                     ),
                   ),
                 ),
+                // Container(
+                //   width: 120,
+                //   height: 120,
+                //   decoration: const BoxDecoration(
+                //     shape: BoxShape.circle,
+                //     image: DecorationImage(
+                //       fit: BoxFit.cover,
+                //       image: AssetImage(
+                //         'assets/img_profile.png',
+                //       ),
+                //     ),
+                //   ),
+                // ),
                 const SizedBox(
                   height: 16,
                 ),
                 Text(
-                  'Shayna Hana',
+                  'Passport/ID Card',
                   style: blackTextStyle.copyWith(
                     fontSize: 18,
                     fontWeight: semibold,
@@ -87,18 +86,19 @@ class SignUpSetProfilePage extends StatelessWidget {
                 const SizedBox(
                   height: 30,
                 ),
-                const CustomFormField(
-                  title: 'Set PIN (6 digit number)',
-                  obscureText: true,
-                ),
                 const SizedBox(
-                  height: 30,
+                  height: 50,
                 ),
                 CustomFiledButton(
                   title: "Continue",
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sign-up-set-ktp');
-                  },
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                CustomTextButton(
+                  title: "Skip for now",
+                  onPressed: () {},
                 ),
               ],
             ),
