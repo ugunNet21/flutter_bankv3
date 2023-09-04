acde72f33665c4258513f8bcc1651138ac1a78bf
gugun gunawan <gugun.net21@gmail.com>
2023-09-04 11:12:36 +0700

    build profile

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -69,6 +69,81 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          buildProfile(),
+          // SizedBox(
+          //   height: 40,
+          // ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Shanahan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_profile.png',
+                ),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
